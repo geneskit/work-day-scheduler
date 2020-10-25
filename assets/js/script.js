ff05b7a5e19fe7bcfcdea357e8c07d04a11a5b97
Emily Thai <emilythai@Emilys-MacBook-Air-2.local>
2020-10-24 20:38:49 -0700

    fixed date and time bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
-// function displayTime() {
-//   var currentTime = moment().format("MM/DD/YYYY hh:mm:ss a");
-//   document.getElementById("current-time").innerText = currentTime;
-// }
+function displayTime() {
+    var currentDay = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+    var $currentT = $("#currentDay");
+    $currentT.text(currentDay);
+}
 
-// setInterval(displayTime, 1000);
+setInterval(displayTime, 1000);
 
-// var currentDay = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
-//     var $currentT = $("#currentDay");
-//     $currentT.text(currentDay);
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
 
+Array.from(rows).forEach(row => {
+    let
+        rowIdString = row.id,
+        rowHour;
+    if (rowIdString) {
+        rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+        if (currentHour === rowHour) {
+            setColor(row, "red");
+        }
+        else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+            setColor(row, "green")
+        }
+        else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+            setColor(row, "lightgrey");
+        }
+        else {
+            setColor(row, "white");
+        }
+    }
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
