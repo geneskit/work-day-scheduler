23811e4284443fed4a77dfd5ca69608631f3d91a
Emily Thai <emilythai@Emilys-MacBook-Air-2.local>
2020-10-24 19:37:08 -0700

    display time and day bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-function displayTime() {
-    var currentTime = moment().format("MM/DD/YYYY hh:mm:ss a");
-    document.getElementById("current-time").innerText = currentTime;
-  }
-  
-  setInterval(displayTime, 1000);
+// function displayTime() {
+//   var currentTime = moment().format("MM/DD/YYYY hh:mm:ss a");
+//   document.getElementById("current-time").innerText = currentTime;
+// }
+
+// setInterval(displayTime, 1000);
+
+// var currentDay = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+//     var $currentT = $("#currentDay");
+//     $currentT.text(currentDay);
+
